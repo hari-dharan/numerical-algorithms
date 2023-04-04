e5ce3e5a52bcad60d517e3b1cadaec685d76f235
Hari <skcharis77@gmail.com>
2023-04-04 19:27:02 +0800

    add BackwardSubstitution.m

---
+++ matlab/BackwardSubstitution.m
@@ -0,0 +1,13 @@
+function x = BackwardSubstitution(A)
+%BACKWARDSUBSTITUTION Backward substitution of an augmented upper triangular matrix.
+n = size(A,1);
+x = zeros(n,1);
+x(n) = A(n,n+1)/A(n,n);
+for i=n-1:-1:1
+    x(i) = A(i,n+1);
+    for j=i+1:n
+        x(i) = x(i) - A(i,j)*x(j);
+    end
+    x(i) = x(i)/A(i,i);
+end
+end
