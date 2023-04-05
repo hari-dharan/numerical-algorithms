cb4c3b719065aa72abadac8ea2989f45891b28c4
Hari <skcharis77@gmail.com>
2023-04-05 10:47:46 +0800

    add QRFactorizationByHouseholder.m

---
+++ matlab/QRFactorizationByHouseholder.m
@@ -0,0 +1,11 @@
+function [Q, R] = QRFactorizationByHouseholder(A)
+%QRFACTORIZATIONBYHOUSEHOLDER Full QR Factorization using Householder transformations.
+[m,n] = size(A);
+Q = Householder(A(:,1), 0);
+R = Q*A;
+for i=2:n
+    HH = Householder(R(i:m,i:n), i-1);
+    Q = Q*HH;
+    R = HH*R;
+end
+end
