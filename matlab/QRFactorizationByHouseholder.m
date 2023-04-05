db7341e357c7a80a00468c6d1f57640f14dd95a1
Hari <skcharis77@gmail.com>
2023-04-05 11:08:10 +0800

    debug QRFactorizationByHouseholder.m

--- matlab/QRFactorizationByHouseholder.m
+++ matlab/QRFactorizationByHouseholder.m
@@ -4,7 +4,7 @@ function [Q, R] = QRFactorizationByHouseholder(A)
 Q = Householder(A(:,1), 0);
 R = Q*A;
 for i=2:n
-    HH = Householder(R(i:m,i:n), i-1);
+    HH = Householder(R(i:m,i), i-1);
     Q = Q*HH;
     R = HH*R;
 end
