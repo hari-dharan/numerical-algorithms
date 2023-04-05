d47a42af525e2bab87c4f7d38cf6733a8a518ab7
Hari <skcharis77@gmail.com>
2023-04-05 08:26:02 +0800

    add docs to PowerIteration.m

--- matlab/PowerIteration.m
+++ matlab/PowerIteration.m
@@ -1,13 +1,15 @@
-function [eigvec, eigval, n_iter] = PowerIteration(A, v, tol)
-% v_k is v at k-th iteration and v_k1 is v at k+1-th iteration
+function [eigvec, eigval] = PowerIteration(A, v, tol)
+%POWERITERATION Find eigenvector and eigenvalue of A.
+%   Power Iteration returns the largest eigenvalue and corresponding eigenvector.
+%   This iteration runs while the error between updates is greater than tol.
+%   v_k is v at k-th iteration and v_k1 is v at k+1-th iteration.
 v_k = zeros(size(v));
 v_k1 = v;
-n_iter = 0;
 while (norm(v_k1 - v_k) / norm(v_k)) > tol
     v_k = v_k1;
     w = A*v_k1;
     v_k1 = w/max(w);
-    n_iter = n_iter + 1;
 end
 eigvec = v_k1;
 eigval = max(w);
+end
